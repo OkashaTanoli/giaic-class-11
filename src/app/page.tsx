8a7808b8598df816ffd948e48b6ad7babded0be3
OkashaTanoli <okashatanoli12345@gmail.com>
2024-12-07 20:46:44 +0500

    changed logo to logo12

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <div className="bg-red-500 py-2 font-bold text-4xl">
         <div className="flex justify-between bg-green-400 w-[95%] 2xl:w-[1500px] mx-auto">
-          <h1>logo1</h1>
+          <h1>logo12</h1>
           <ul className="flex gap-5">
             <li>1</li>
             <li>2</li>
