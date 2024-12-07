e5610dfd9fc0225df7b2cf457eaff65643b0a95f
OkashaTanoli <okashatanoli12345@gmail.com>
2024-12-07 20:39:07 +0500

    test

---
+++ src/app/page.tsx
@@ -0,0 +1,17 @@
+"use client"
+
+export default function Home() {
+
+  return (
+    <div className="bg-red-500 py-2 font-bold text-4xl">
+        <div className="flex justify-between bg-green-400 w-[95%] 2xl:w-[1500px] mx-auto">
+          <h1>logo</h1>
+          <ul className="flex gap-5">
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+          </ul>
+        </div>
+    </div>
+  );
+}
